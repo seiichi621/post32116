2c16982921119ff4dcec2127d279bfbea5c60dfb
seiichi621 <sei-0941-seiichi5@softbank.ne.jp>
2021-01-23 13:36:13 +0900

    コントローラ,モデル,データ設計

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,30 @@
+class ArticlesController < ApplicationController
+  
+  # 記事の一覧表示
+  def index
+  end
+
+  # 記事の表示
+  def show
+  end
+
+  # 記事の作成
+  def new 
+  end
+
+  # 記事の登録
+  def create
+  end
+
+  # 記事の編集
+  def edit
+  end
+
+  # 記事の更新
+  def update
+  end
+
+  # 記事の削除
+  def destroy
+  end
+end
