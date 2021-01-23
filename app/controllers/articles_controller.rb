3cc162c900c10f797787e96c9c1a2cb71f0dbf65
seiichi621 <sei-0941-seiichi5@softbank.ne.jp>
2021-01-24 07:21:36 +0900

    投稿機能

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,19 +1,38 @@
 class ArticlesController < ApplicationController
-  
+
   # 記事の一覧表示
   def index
+    @articles = Article.all
   end
 
   # 記事の表示
   def show
+    @article = Article.find(params[:id])
   end
 
   # 記事の作成
   def new 
+    @article = Article.new
   end
 
   # 記事の登録
   def create
+    # articleモデルの属性の初期化
+    @article = Article.new(article_params)
+
+    # DBに登録できた場合
+    
+    if @article.save
+
+      # 記事ページに遷移する
+      redirect_to @article
+
+    # DBに登録できなかった場合
+    else
+      # 作成ページに遷移する
+      render 'new'
+    end
+
   end
 
   # 記事の編集
@@ -27,4 +46,9 @@ class ArticlesController < ApplicationController
   # 記事の削除
   def destroy
   end
+
+  private
+    def article_params
+        params.require(:article).permit(:title, :text)
+    end
 end
